979b4c1e20d2b4da209e0ff1c6e91782599b2d36
luxerama <vincent@siebert.im>
2013-03-17 12:06:49 +0000

    adding nvm source and using node v0.8.8

--- zsh/boxen.zsh
+++ zsh/boxen.zsh
@@ -3,3 +3,10 @@ if [[ -a /opt/boxen/env.sh ]]
 then
   source /opt/boxen/env.sh
 fi
+
+# Load global nvm
+if [[ -a /opt/boxen/nvm/nvm.sh ]]
+then
+  source /opt/boxen/nvm/nvm.sh
+  nvm use 0.8.8
+fi
